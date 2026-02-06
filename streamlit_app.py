2b170b99b05d69fb0899ab71923448a1a5b4b947
Cristhy Viera <162900018+CristhyViera@users.noreply.github.com>
2026-02-06 16:13:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,13 +44,13 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
         # Buscamos el valor de SEARCH_ON correspondiente a la fruta elegida
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         
         # Petición a la API usando el valor de SEARCH_ON
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        fruityvice = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         
         # Muestra la información nutricional en un dataframe
         if smoothiefroot_response.status_code == 200:
